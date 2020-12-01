b3de126694217c8b72942972b620fc5492fbed8d
WizardLemon <nemanjalekic.ns@gmail.com>
2020-12-01 20:44:57 +0100

    Hide secret key

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from profiles_project.env import dev
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1$ef5s#02llwbaa$1ut4x4gvzuk)#j#)m#zt5eoy8t9_ilxgtb'
+SECRET_KEY = dev.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
