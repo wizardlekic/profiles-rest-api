034632cb043b937546e797d310347dd629e1786c
WizardLemon <nemanjalekic.ns@gmail.com>
2021-12-21 21:22:46 +0100

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,12 +2,16 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloAPIView(APIView):
     """Test API View"""
+
     serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
@@ -56,6 +60,7 @@ class HelloAPIView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
+
     serializer_class = serializers.HelloSerializer
 
     def list(self, request):
@@ -104,3 +109,12 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
