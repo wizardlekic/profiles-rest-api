4dcfa18ad6b5f7868e81782e01316b742faaecb3
WizardLemon <nemanjalekic.ns@gmail.com>
2021-12-25 21:41:25 +0100

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -12,5 +12,6 @@ router.register('profile', views.UserProfileViewSet) # base_name implied by qs
 
 urlpatterns = [
     path('hello-view/', views.HelloAPIView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
